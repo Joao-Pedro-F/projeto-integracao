d04930c34875a76d763744a96bbd8d58d17cd521
Joao-Pedro-F <jpfa0809@gmail.com>
2026-03-16 17:10:22 -0300

    fix banco de dados neondb  demorando para responder

--- app_laravel.py
+++ app_laravel.py
@@ -1,9 +1,32 @@
 import os
-
-# Arquivo principal da aplicacao (tipo Laravel)
+import time
 from flask import Flask
 from flask_cors import CORS
+from sqlalchemy import create_engine 
 from routes.web import registrar_rotas
+
+# func para ativar o NEONDB 
+def aguardar_banco():
+    url = os.getenv("DATABASE_URL")
+    if not url:
+        return
+    
+    print("Verificando se o NeonDB está on")
+    for i in range(5):  # Tenta por até 20 segundos (5 vezes x 4 segundos)
+        try:
+            #  testar conexao
+            engine = create_engine(url)
+            with engine.connect() as conn:
+                print("NeonDB está online!")
+                return
+        except Exception:
+            print(f" Banco off (tentativa {i+1}/5)... esperando 4s")
+            time.sleep(4)
+    print(" Prosseguindo mesmo sem confirmação do banco...")
+
+# Chama a função antes de carregar as rotas
+aguardar_banco()
+# ------------------------------------
 
 # Criar aplicacao Flask
 app = Flask(__name__)
@@ -16,5 +39,6 @@ registrar_rotas(app)
 # Executar aplicacao
 if __name__ == '__main__':
     port = int(os.getenv("PORT", "5000"))
+    # No Render, é melhor deixar o debug como False em produção
     debug = os.getenv("FLASK_DEBUG", "").strip().lower() in ("1", "true", "yes")
     app.run(host="0.0.0.0", port=port, debug=debug)
